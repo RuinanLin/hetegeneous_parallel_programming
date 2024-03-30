ca8bee4dc685a9edff2d24a6c2383d1c88392f2c
Ruinan Lin <rlin2@dt-login01.delta.ncsa.illinois.edu>
2024-03-29 20:12:51 -0500

    next multigpu

--- multigpu.cu
+++ multigpu.cu
@@ -1,5 +1,3 @@
-// Copyright (c) 2020 MIT
-// Author: Xuhao Chen
 #include <cub/cub.cuh>
 //#include "edgelist.h"
 #include "graph_gpu.h"
@@ -36,18 +34,19 @@ class Type1SubGraph {
     eidType *row_pointers;                // row pointers of CSR format, starting point only inner
 
     std::vector<directedEdge> temp_edges; // for the process of generating, allowing start from outer
-    vidType *edge_counters;               // counts how many edges are from a particular vertex
-    eidType this_num_edges;               // counts how many edges in this subgraph
+    int creation_finished;                // whether the subgraph has finished creating
 
   public:
     void init(Graph &g, int num_partitions, int this_partition_idx);
     void destroy();
     void add_edge(vidType from, vidType to);
+    vidType get_out_degree(vidType u);
+    vidType N(vidType u, vidType n);
 };
 
 // initialize the subgraph
 void Type1SubGraph::init(Graph &g, int num_partitions, int this_partition_idx) :
-    super_num_vertices(g.V()), num_partitions(num_partitions), this_partition_idx(this_partition_idx), this_num_edges(0)
+    super_num_vertices(g.V()), num_partitions(num_partitions), this_partition_idx(this_partition_idx), creation_finished(0)
 {
   // initialize its own logFile
   std::string file_name = "Type1SubGraph_log" + std::to_string(this_partition_idx) + ".txt";
@@ -73,10 +72,9 @@ void Type1SubGraph::init(Graph &g, int num_partitions, int this_partition_idx) :
   // initialize the CSR
   logFile << "\tAllocate row_pointers ...\n";
   edges = NULL;   // no edges now
-  row_pointers = (eidType *)malloc(this_num_vertices * sizeof(eidType));
-  edge_counters = (vidType *)malloc(this_num_vertices * sizeof(vidType));
-  for (vidType u = 0; u < this_num_vertices; u++) // clear the counters
-    edge_counters[u] = 0;
+  row_pointers = (eidType *)malloc((this_num_vertices + 1) * sizeof(eidType));
+  for (eidType u = 0; u < this_num_vertices + 1; u++)
+    row_pointers[u] = 0;
 
   // finish initialization and exit
   logFile << "Initialization succeeded!\n";
@@ -89,6 +87,7 @@ void Type1SubGraph::destroy()
 
   // free the "edges_from_outside" and "edges_to_outside"
   logFile << "\tFree the allocated memory ...\n";
+  free(edges);
   free(row_pointers);
   logFile << "\tAllocated memory freed!\n";
 
@@ -101,8 +100,7 @@ void Type1SubGraph::destroy()
 void Type1SubGraph::add_edge(vidType from, vidType to)
 {
   // refresh the counter of 'from'
-  edge_counters[from]++;
-  this_num_edges++;
+  row_pointers[from - start_vertex_idx]++;
 
   // create a pair
   directedEdge edge(from, to);
@@ -112,7 +110,61 @@ void Type1SubGraph::add_edge(vidType from, vidType to)
 // reorder the temp_edges and create the final CSR format
 void Type1SubGraph::reduce()
 {
-  
+  // allocate memory for 'edges'
+  edges = (vidType *)malloc(temp_edges.size() * sizeof(vidType));
+
+  // perform scanning on 'row_pointers', thus getting the indices
+  for (vidType counter_idx = 0; counter_idx < this_num_vertices; counter_idx++)
+    row_pointers[counter_idx + 1] += row_pointers[counter_idx];
+
+  // pop the edges from back one by one and place it to the correct place
+  while (temp_edges.size() > 0)
+  {
+    // pop the last pair out
+    directedEdge edge = temp_edges[temp_edges.size() - 1];
+    temp_edges.pop_back();
+
+    // put it into the right place in 'edges'
+    edges[--row_pointers[edge.first - start_vertex_idx]] = edge.second;
+  }
+
+  // pull up the 'creation_finished' flag
+  creation_finished = 1;
+
+  // print the result into the logFile
+  logFile << "Type1SubGraph " << this_partition_idx << " has finished reducing!\n";
+  for (vidType u = start_vertex_idx; u < start_vertex_idx + this_num_vertices; u++)
+  {
+    logFile << "\t" << u << ": ";
+    vidType u_deg = this->get_out_degree(u);
+    for (vidType v_idx = 0; v_idx < u_deg; v_idx++)
+      logFile << this->N(u, v_idx) << " ";
+    logFile << "\n";
+  }
+}
+
+// get the out-degree of vertex u
+vidType Type1SubGraph::get_out_degree(vidType u)
+{
+  // the function must be called after the creation of the subgraph has finished
+  if (creation_finished == 0)
+  {
+    std::cout << "Error! Subgraph creation has not finished, but 'get_out_degree()' is called!\n";
+    exit(-1);
+  }
+  return row_pointers[u - start_vertex_idx + 1] - row_pointers[u - start_vertex_idx];
+}
+
+// get the n-th neighbor of u
+vidType Type1SubGraph::N(vidType u, vidType n)
+{
+  // the function must be called after the creation of the subgraph has finished
+  if (creation_finished == 0)
+  {
+    std::cout << "Error! Subgraph creation has not finished, but 'N()' is called!\n";
+    exit(-1);
+  }
+  return edges[row_pointers[u - start_vertex_idx] + n];
 }
 
 /**************************************** Definition of TCSolver ***************************************/
